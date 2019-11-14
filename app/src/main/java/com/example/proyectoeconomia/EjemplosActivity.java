959d3dc89db3ea9d344890ee63ab429f8bd4ff4b
edyson10 <edysonfabian@hotmail.com>
2019-11-14 17:24:10 -0500

    Se realizo modificaciones

--- app/src/main/java/com/example/proyectoeconomia/EjemplosActivity.java
+++ app/src/main/java/com/example/proyectoeconomia/EjemplosActivity.java
@@ -7,11 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.proyectoeconomia.Ejercicios.EjercicioActivity;
-import com.example.proyectoeconomia.Solucion.SolucionInteresActivity;
 
 public class EjemplosActivity extends AppCompatActivity {
 
